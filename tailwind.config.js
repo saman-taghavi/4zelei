50ae3354af51d41b1a29234747f36deac49192cd
Saman Taghavi <samantaghavisani@gmail.com>
2022-06-15 02:15:41 +0430

    ADD daisy-ui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
-}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e56116",
+
+          secondary: "#3B7FFF",
+
+          accent: "#DD415A",
+
+          neutral: "#DFE0DF",
+
+          "base-100": "#2A303C",
+
+          info: "#64BAAA",
+
+          success: "#00A12B",
+
+          warning: "#FBBD23",
+
+          error: "#9D2200",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
