63b66e92aafbacf236cd045f19c2c767d3208a3d
Saman Taghavi <samantaghavisani@gmail.com>
2022-06-18 22:06:27 +0430

    ADD other pages

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,13 @@ definePageMeta({
 </script>
 
 <template>
-  <div>
-    hi
+
+  <div class="flex flex-col gap-3  p-4">
+    <input type="text" placeholder="Default" class="input input-bordered w-full"> <input type="text"
+      placeholder="Primary" class="input input-primary input-bordered w-full"> <input type="text"
+      placeholder="Secondary" class="input input-secondary input-bordered w-full"> <input type="text"
+      placeholder="Accent" class="input input-accent input-bordered w-full">
+    <input type="text" placeholder="Primary" class="input input-primary input-bordered ">
+    <button class="btn mt-3 btn-primary">Hello daisyUI</button>
   </div>
 </template>
---
+++ pages/services.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'default',
+})
+</script>
+
+<template>
+
+  <div class="flex flex-col gap-3  p-4">
+    this is about services
+  </div>
+</template>
