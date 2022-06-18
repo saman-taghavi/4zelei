adacaa0edd948b854df45f2d0887265151e7a62b
Saman Taghavi <samantaghavisani@gmail.com>
2022-06-18 22:06:15 +0430

    use nuxt-link

--- components/tHeader.vue
+++ components/tHeader.vue
@@ -1,19 +1,19 @@
 <template>
     <header class="text-gray-600 body-font  ">
         <div class=" container mx-auto flex flex-wrap py-5  flex-col md:flex-row items-center">
-            <div class="avatar mx-3">
+            <nuxt-link to="/" class="avatar mx-3">
                 <div class="w-32 h-24 rounded bg-[url('4ze.jpg')]  bg-cover">
                 </div>
-            </div>
+            </nuxt-link>
             <nav class="md:ml-auto my-3 md:my-3 md:pl-4 flex flex-wrap items-center text-base justify-center">
-                <button
+                <nuxt-link to="/services"
                     class="mx-2 bg-primary  backdrop-blur-sm hover:ring-2 rounded-sm ring-offset-2 ring-primary  text-white hover:text-primary hover:bg-white btn  ">
-                    <a>خدمات ما</a>
-                </button>
-                <button
+                    خدمات ما
+                </nuxt-link>
+                <nuxt-link to="/about"
                     class="mx-2 bg-primary  backdrop-blur-sm hover:ring-2 rounded-sm ring-offset-2 ring-primary  text-white hover:text-primary hover:bg-white btn  ">
-                    <a>درباره ما</a>
-                </button>
+                    درباره ما
+                </nuxt-link>
             </nav>
         </div>
     </header>
