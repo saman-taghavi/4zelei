07e49c6300669cfb9b27bb6990f9445e4dd1d6b1
Saman Taghavi <samantaghavisani@gmail.com>
2022-06-18 22:00:06 +0430

    FIX logo size to a point

--- components/tHeader.vue
+++ components/tHeader.vue
@@ -2,8 +2,7 @@
     <header class="text-gray-600 body-font  ">
         <div class=" container mx-auto flex flex-wrap py-5  flex-col md:flex-row items-center">
             <div class="avatar mx-3">
-                <div class="w-20 rounded">
-                    <img src="4ze.jpg" alt="Tailwind-CSS-Avatar-component" />
+                <div class="w-32 h-24 rounded bg-[url('4ze.jpg')]  bg-cover">
                 </div>
             </div>
             <nav class="md:ml-auto my-3 md:my-3 md:pl-4 flex flex-wrap items-center text-base justify-center">
